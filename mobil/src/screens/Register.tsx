73824663334405e66a2e575cef3bb450339861e1
Rabiaparlak <68352029+Rabiaparlak@users.noreply.github.com>
2023-06-01 00:40:09 +0300

    Kayıt sayfasına Api'dan şehir çekme yapıldı.

--- mobil/src/screens/Register.tsx
+++ mobil/src/screens/Register.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, ImageBackground, StyleSheet } from "react-native";
 import Layout from "../../constants/Layout";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -21,186 +21,209 @@ import { ScrollView } from "react-native-gesture-handler";
 
 const Register = () => {
   const navigation = useNavigation();
-  const countries = ["Egypt", "Canada", "Australia", "Ireland"];
+  const [isCitys, setIsCitys] = useState([]);
+  type city={
+    id:number;
+    title:string;
+  }
+  useEffect(() => {
+    fetch("https://api.countrystatecity.in/v1/countries")
+      .then((response) => response.json())
+      .then((response) => setIsCitys(response));
+  }, []);
   return (
     <SafeAreaView style={styles.SafeAreaView}>
       <ScrollView>
-
-    
-      <View style={styles.Page}>
-        <ImageBackground
-          source={require("../../assets/images/backIcon.png")}
-          style={styles.BackgroundImage}
-        >
-          <Image source={require("../../assets/images/title.png")} />
-        </ImageBackground>
-      </View>
-      <View style={styles.FormArea}>
-        <View style={styles.InputArea}>
-          <Text
-            style={{ alignSelf: "center" }}
-            fontSize="3xl"
-            bold
-            marginBottom={5}
+        <View style={styles.Page}>
+          <ImageBackground
+            source={require("../../assets/images/backIcon.png")}
+            style={styles.BackgroundImage}
           >
-            Kayıt Ol
-          </Text>
-          <ScrollView>
-            <SelectDropdown
-              data={countries}
-              defaultButtonText="İl Seç"
-              buttonStyle={{
-                backgroundColor: "white",
-                borderBottomColor: "#878BFF",
-                borderBottomWidth: 1,
-                width: Layout.window.width * 0.7,
-            
-              }}
-              
-              dropdownOverlayColor="rgba(0,0,0,0.7)"
-              buttonTextStyle={{fontSize:12,position:"absolute", right:5}}
-              onSelect={(selectedItem, index) => {
-                console.log(selectedItem, index);
-              }}
-              buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem;
-              }}
-              rowTextForSelection={(item, index) => {
-                return item;
-              }}
-            />
-            <SelectDropdown
-              data={countries}
-              defaultButtonText="İlçe Seç"
-              dropdownOverlayColor="rgba(0,0,0,0.7)"
-              buttonTextStyle={{fontSize:12,position:"absolute", right:5}}
-              buttonStyle={{
-                backgroundColor: "white",
-                borderBottomColor: "#878BFF",
-                borderBottomWidth: 1,
-                width: Layout.window.width * 0.7,
-              }}
-              onSelect={(selectedItem, index) => {
-                console.log(selectedItem, index);
-              }}
-              buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem;
-              }}
-              rowTextForSelection={(item, index) => {
-                return item;
-              }}
-            />
-            <SelectDropdown
-              data={countries}
-              defaultButtonText="Mahalle Seç"
-              dropdownOverlayColor="rgba(0,0,0,0.7)"
-              buttonTextStyle={{fontSize:12, position:"absolute",right:5}}
-              buttonStyle={{
-                backgroundColor: "white",
-                borderBottomColor: "#878BFF",
-                borderBottomWidth: 1,
-                width: Layout.window.width * 0.7,
-                
-              }}
-              onSelect={(selectedItem, index) => {
-                console.log(selectedItem, index);
-              }}
-              buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem;
-              }}
-              rowTextForSelection={(item, index) => {
-                return item;
-              }}
-            />
-
-            <Input
-              variant="underlined"
-              numberOfLines={4}
-              maxLength={40}
-              placeholder="İşletme İsmi"
-              InputLeftElement={
-                <Icon
-                  as={<SimpleLineIcons name="bag" />}
-                  marginRight={2}
-                  size={5}
-                  ml="2"
-                  color="#000000"
-                />
-              }
-            />
-            <Input
-              variant="underlined"
-              numberOfLines={4}
-              maxLength={40}
-              placeholder="E-Posta"
-              InputLeftElement={
-                <Icon
-                  as={<Feather name="mail" />}
-                  marginRight={2}
-                  size={5}
-                  ml="2"
-                  color="#000000"
-                />
-              }
-            />
-            <Input
-              variant="underlined"
-              numberOfLines={4}
-              keyboardType="numeric"
-              maxLength={40}
-              placeholder="Şifre"
-              InputLeftElement={
-                <Icon
-                  as={<AntDesign name="unlock" />}
-                  marginRight={2}
-                  size={5}
-                  ml="2"
-                  color="#000000"
-                />
-              }
-            />
-            <Input
-              variant="underlined"
-              numberOfLines={4}
-              keyboardType="numeric"
-              maxLength={40}
-              placeholder="Şifre Tekrar"
-              InputLeftElement={
-                <Icon
-                  as={<AntDesign name="unlock" />}
-                  marginRight={2}
-                  size={5}
-                  ml="2"
-                  color="#000000"
-                />
-              }
-            />
-          </ScrollView>
+            <Image source={require("../../assets/images/title.png")} />
+          </ImageBackground>
         </View>
-
-        <VStack marginTop={3} space={5}>
-        <CheckButton 
-          navigation={navigation}
-          navigate="Login"
-          text="Kayıt Ol"
-          color="#FF7B00"
-          />
-          <HStack space={3} alignItems={"center"}>
-            <View style={styles.Line}></View>
-            <Text bold fontSize={"md"} color={"#878BFF"}>
-              ya da
+        <View style={styles.FormArea}>
+          <View style={styles.InputArea}>
+            <Text
+              style={{ alignSelf: "center" }}
+              fontSize="3xl"
+              bold
+              marginBottom={5}
+            >
+              Kayıt Ol
             </Text>
-            <View style={styles.Line}></View>
-          </HStack>
-          <CheckButton
-            navigation={navigation}
-            navigate="Login"
-            text="Giriş Yap"
-            color="#00C599"
-          />
-        </VStack>
-      </View>
-        </ScrollView>
+            <ScrollView>
+              {isCitys.map((city:any) => {
+                return (
+                  <VStack key={city.id}>
+                    <Text color={"black"}>{city.title}</Text>
+                  </VStack>
+                );
+              })}
+              <SelectDropdown
+                data={isCitys}
+                defaultButtonText={"İl Seç"}
+                buttonStyle={{
+                  backgroundColor: "white",
+                  borderBottomColor: "#878BFF",
+                  borderBottomWidth: 1,
+                  width: Layout.window.width * 0.7,
+                }}
+                dropdownOverlayColor="rgba(0,0,0,0.7)"
+                buttonTextStyle={{
+                  fontSize: 12,
+                  position: "absolute",
+                  right: 5,
+                }}
+                onSelect={(selectedItem, index) => {
+                  console.log(selectedItem, index);
+                }}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                  return selectedItem;
+                }}
+                rowTextForSelection={(item, index) => {
+                  return item;
+                }}
+              />
+              <SelectDropdown
+                data={isCitys}
+                defaultButtonText="İlçe Seç"
+                dropdownOverlayColor="rgba(0,0,0,0.7)"
+                buttonTextStyle={{
+                  fontSize: 12,
+                  position: "absolute",
+                  right: 5,
+                }}
+                buttonStyle={{
+                  backgroundColor: "white",
+                  borderBottomColor: "#878BFF",
+                  borderBottomWidth: 1,
+                  width: Layout.window.width * 0.7,
+                }}
+                onSelect={(selectedItem, index) => {
+                  console.log(selectedItem, index);
+                }}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                  return selectedItem;
+                }}
+                rowTextForSelection={(item, index) => {
+                  return item;
+                }}
+              />
+              <SelectDropdown
+                data={isCitys}
+                defaultButtonText="Mahalle Seç"
+                dropdownOverlayColor="rgba(0,0,0,0.7)"
+                buttonTextStyle={{
+                  fontSize: 12,
+                  position: "absolute",
+                  right: 5,
+                }}
+                buttonStyle={{
+                  backgroundColor: "white",
+                  borderBottomColor: "#878BFF",
+                  borderBottomWidth: 1,
+                  width: Layout.window.width * 0.7,
+                }}
+                onSelect={(selectedItem, index) => {
+                  console.log(selectedItem, index);
+                }}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                  return selectedItem;
+                }}
+                rowTextForSelection={(item, index) => {
+                  return item;
+                }}
+              />
+
+              <Input
+                variant="underlined"
+                numberOfLines={4}
+                maxLength={40}
+                placeholder="İşletme İsmi"
+                InputLeftElement={
+                  <Icon
+                    as={<SimpleLineIcons name="bag" />}
+                    marginRight={2}
+                    size={5}
+                    ml="2"
+                    color="#000000"
+                  />
+                }
+              />
+              <Input
+                variant="underlined"
+                numberOfLines={4}
+                maxLength={40}
+                placeholder="E-Posta"
+                InputLeftElement={
+                  <Icon
+                    as={<Feather name="mail" />}
+                    marginRight={2}
+                    size={5}
+                    ml="2"
+                    color="#000000"
+                  />
+                }
+              />
+              <Input
+                variant="underlined"
+                numberOfLines={4}
+                keyboardType="numeric"
+                maxLength={40}
+                placeholder="Şifre"
+                InputLeftElement={
+                  <Icon
+                    as={<AntDesign name="unlock" />}
+                    marginRight={2}
+                    size={5}
+                    ml="2"
+                    color="#000000"
+                  />
+                }
+              />
+              <Input
+                variant="underlined"
+                numberOfLines={4}
+                keyboardType="numeric"
+                maxLength={40}
+                placeholder="Şifre Tekrar"
+                InputLeftElement={
+                  <Icon
+                    as={<AntDesign name="unlock" />}
+                    marginRight={2}
+                    size={5}
+                    ml="2"
+                    color="#000000"
+                  />
+                }
+              />
+            </ScrollView>
+          </View>
+
+          <VStack marginTop={3} space={5}>
+            <CheckButton
+              navigation={navigation}
+              navigate="Login"
+              text="Kayıt Ol"
+              color="#FF7B00"
+            />
+            <HStack space={3} alignItems={"center"}>
+              <View style={styles.Line}></View>
+              <Text bold fontSize={"md"} color={"#878BFF"}>
+                ya da
+              </Text>
+              <View style={styles.Line}></View>
+            </HStack>
+            <CheckButton
+              navigation={navigation}
+              navigate="Login"
+              text="Giriş Yap"
+              color="#00C599"
+            />
+          </VStack>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
